7ed83f9ea29943a12e6b7438978e54903b70defe
Andrew Vasilyev <me@retran.me>
2024-11-03 22:07:06 +0100

    add solution for problem 0657

---
+++ LeetCode.Problem0657/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public bool JudgeCircle(string moves)
+    {
+        int l = 0, r = 0, u = 0, d = 0;
+
+        for (int i = 0; i < moves.Length; i++)
+        {
+            if (moves[i] == 'R')
+            {
+                r++;
+            }
+            else if (moves[i] == 'L')
+            {
+                l++;
+            }
+            else if (moves[i] == 'U')
+            {
+                u++;
+            }
+            else
+            {
+                d++;
+            }
+        }
+
+        return l == r && u == d;
+    }
+}
