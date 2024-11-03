4ef2af60e6805b5288055dea943f294a6c69792e
Andrew Vasilyev <me@retran.me>
2024-11-03 19:44:31 +0100

    add solutions for problems 0389 and 0459

---
+++ LeetCode.Problem0389/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+
+var solution = new Solution();
+
+Debug.Assert(solution.FindTheDifference("abcd", "abcde") == 'e');
+Debug.Assert(solution.FindTheDifference("", "y") == 'y');
+
+public class Solution
+{
+    public char FindTheDifference(string s, string t)
+    {
+        var map = new Dictionary<char, int>();
+
+        foreach (var c in s)
+        {
+            if (!map.TryGetValue(c, out var count))
+            {
+                count = 0;
+            }
+            map[c] = count + 1;
+        }
+
+        foreach (var c in t)
+        {
+            if (!map.TryGetValue(c, out var count) || count == 0)
+            {
+                return c;
+            }
+
+            map[c] = count - 1;
+        }
+
+        return (char)0;
+    }
+}
--- LeetCode.Problem0482/Program.cs
+++ LeetCode.Problem0482/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Text;
 
 var solution = new Solution();
