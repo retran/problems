b05a71a50a3f659ead9358f964775d94629e14f8
Andrew Vasilyev <me@retran.me>
2024-11-03 20:11:02 +0100

    add solution for problem 1502

---
+++ LeetCode.Problem1502/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public bool CanMakeArithmeticProgression(int[] arr)
+    {
+        Array.Sort(arr);
+        for (int i = 1; i < arr.Length - 1; i++)
+        {
+            int prevDiff = Math.Abs(arr[i] - arr[i - 1]);
+            int nextDiff = Math.Abs(arr[i] - arr[i + 1]);
+            
+            if (prevDiff != nextDiff)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
