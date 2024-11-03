90a7e2e94b50277e3af37ab4c49cc07dee8dad4b
Andrew Vasilyev <me@retran.me>
2024-11-03 21:35:37 +0100

    add solution for problem 0709

---
+++ LeetCode.Problem0709/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public string ToLowerCase(string s)
+    {
+        char[] chars = s.ToCharArray();
+
+        for (int i = 0; i < chars.Length; i++)
+        {
+            if (chars[i] >= 'A' && chars[i] <= 'Z')
+            {
+                chars[i] = (char)(chars[i] + 32);
+            }
+        }
+
+        return new string(chars);
+    }
+}
