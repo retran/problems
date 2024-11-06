0ae0e8d1f4b343e7cc8dce4c07a816fe907e0d1a
Andrew Vasilyev <me@retran.me>
2024-11-06 20:30:39 +0100

    add solution for problem 0997

---
+++ LeetCode.Problem0997/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public int FindJudge(int n, int[][] trust)
+    {
+        int[] inbound = new int[n];
+        int[] outbound = new int[n];
+
+        foreach (var relation in trust)
+        {
+            inbound[relation[1] - 1]++;
+            outbound[relation[0] - 1]++;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (outbound[i] == 0 && inbound[i] == n - 1)
+            {
+                return i + 1;
+            }
+        }
+
+        return -1;
+    }
+}
