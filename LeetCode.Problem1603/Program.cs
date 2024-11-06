5c6109c2618174661e3375a66a3b8e9405b7327f
Andrew Vasilyev <me@retran.me>
2024-11-06 19:49:30 +0100

    add solution for problem 1603

---
+++ LeetCode.Problem1603/Program.cs
@@ -0,0 +1,23 @@
+﻿public class ParkingSystem
+{
+    private int[] _slots = new int[3];
+
+    public ParkingSystem(int big, int medium, int small)
+    {
+        _slots[0] = big;
+        _slots[1] = medium;
+        _slots[2] = small;
+    }
+
+    public bool AddCar(int carType)
+    {
+        carType--;
+        if (_slots[carType] > 0)
+        {
+            _slots[carType]--;
+            return true;
+        }
+
+        return false;
+    }
+}
