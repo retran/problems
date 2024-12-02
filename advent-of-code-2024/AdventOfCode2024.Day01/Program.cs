b3878e1c21f3ff381001b6e687a754de815db81a
Andrew Vasilyev <me@retran.me>
2024-12-02 14:38:20 +0100

    Advent of Code 2024, Day 02

--- advent-of-code-2024/AdventOfCode2024.Day01/Program.cs
+++ advent-of-code-2024/AdventOfCode2024.Day01/Program.cs
@@ -1,87 +1,111 @@
 ﻿internal class Program
 {
-    private static void ComputeDistance(string inputFileName, string outputFileName)
-    {
-        var left = new PriorityQueue<int, int>();
-        var right = new PriorityQueue<int, int>();
-
-        long totalDistance = 0;
-
-        using (var sr = new StreamReader(inputFileName))
-        {
-            while (!sr.EndOfStream)
-            {
-                var line = sr.ReadLine();
-                var values = line
-                    .Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
-                    .Select(int.Parse)
-                    .ToArray();
-                left.Enqueue(values[0], values[0]);
-                right.Enqueue(values[1], values[1]);
-            }
-        }
-
-        while (left.Count > 0 && right.Count > 0)
-        {
-            var l = left.Dequeue();
-            var r = right.Dequeue();
-            totalDistance += Math.Abs(r - l);
-        }
-
-        using (var sw = new StreamWriter(outputFileName))
-        {
-            sw.WriteLine(totalDistance);
-        }
-    }
-
-    private static void ComputeSimilarity(string inputFileName, string outputFileName)
-    {
-        var left = new List<int>();
-        var rightFrequencies = new Dictionary<int, int>();
-
-        using (var sr = new StreamReader(inputFileName))
-        {
-            while (!sr.EndOfStream)
-            {
-                var line = sr.ReadLine();
-                var values = line
-                    .Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
-                    .Select(int.Parse)
-                    .ToArray();
-                left.Add(values[0]);
-
-                if (rightFrequencies.ContainsKey(values[1]))
-                {
-                    rightFrequencies[values[1]]++;
-                }
-                else
-                {
-                    rightFrequencies[values[1]] = 1;
-                }
-            }
-        }
-
-        long totalSimilarity = 0;
-
-        foreach (var l in left)
-        {
-            if (rightFrequencies.ContainsKey(l))
-            {
-                totalSimilarity += l * rightFrequencies[l];
-            }
-        }
-
-        using (var sw = new StreamWriter(outputFileName))
-        {
-            sw.WriteLine(totalSimilarity);
-        }
-    }
-
     public static void Main(string[] args)
     {
         ComputeDistance("input_01.txt", "output_01_01.txt");
         ComputeDistance("input_02.txt", "output_01_02.txt");
         ComputeSimilarity("input_01.txt", "output_02_01.txt");
         ComputeSimilarity("input_02.txt", "output_02_02.txt");
+    }
+
+    private static void ComputeDistance(string inputFileName, string outputFileName)
+    {
+        var leftQueue = new PriorityQueue<int, int>();
+        var rightQueue = new PriorityQueue<int, int>();
+        long totalDistance = 0;
+
+        try
+        {
+            using (var reader = new StreamReader(inputFileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (string.IsNullOrWhiteSpace(line))
+                        continue;
+
+                    var values = ParseLine(line);
+                    leftQueue.Enqueue(values[0], values[0]);
+                    rightQueue.Enqueue(values[1], values[1]);
+                }
+            }
+
+            while (leftQueue.Count > 0 && rightQueue.Count > 0)
+            {
+                var leftValue = leftQueue.Dequeue();
+                var rightValue = rightQueue.Dequeue();
+                totalDistance += Math.Abs(rightValue - leftValue);
+            }
+
+            WriteOutput(outputFileName, totalDistance);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error in ComputeDistance: {ex.Message}");
+        }
+    }
+
+    private static void ComputeSimilarity(string inputFileName, string outputFileName)
+    {
+        var leftValues = new List<int>();
+        var rightFrequencies = new Dictionary<int, int>();
+        long totalSimilarity = 0;
+
+        try
+        {
+            using (var reader = new StreamReader(inputFileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (string.IsNullOrWhiteSpace(line))
+                        continue;
+
+                    var values = ParseLine(line);
+                    leftValues.Add(values[0]);
+
+                    if (rightFrequencies.ContainsKey(values[1]))
+                        rightFrequencies[values[1]]++;
+                    else
+                        rightFrequencies[values[1]] = 1;
+                }
+            }
+
+            foreach (var leftValue in leftValues)
+            {
+                if (rightFrequencies.TryGetValue(leftValue, out int frequency))
+                {
+                    totalSimilarity += leftValue * frequency;
+                }
+            }
+
+            WriteOutput(outputFileName, totalSimilarity);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error in ComputeSimilarity: {ex.Message}");
+        }
+    }
+
+    private static int[] ParseLine(string line)
+    {
+        return line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+                   .Select(int.Parse)
+                   .ToArray();
+    }
+
+    private static void WriteOutput(string outputFileName, long result)
+    {
+        try
+        {
+            using (var writer = new StreamWriter(outputFileName))
+            {
+                writer.WriteLine(result);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error writing to {outputFileName}: {ex.Message}");
+        }
     }
 }
---
+++ advent-of-code-2024/AdventOfCode2024.Day02/Program.cs
@@ -0,0 +1,123 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            ProcessReportsAndCountSafeOnes("input_01.txt", "output_01_01.txt", IsReportSafeWithoutAdjustment);
+            ProcessReportsAndCountSafeOnes("input_01.txt", "output_02_01.txt", IsReportSafeWithOneAdjustment);
+            ProcessReportsAndCountSafeOnes("input_02.txt", "output_01_02.txt", IsReportSafeWithoutAdjustment);
+            ProcessReportsAndCountSafeOnes("input_02.txt", "output_02_02.txt", IsReportSafeWithOneAdjustment);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An unexpected error occurred in Main: {ex.Message}");
+        }
+    }
+
+    private static bool IsReportSafe(int[] report, int excludedIndex = -1)
+    {
+        if (report.Length < 2)
+        {
+            return true;
+        }
+
+        int previousDifference = 0;
+
+        for (int i = 0; i < report.Length - 1; i++)
+        {
+            if (i == excludedIndex)
+            {
+                continue;
+            }
+
+            int j = i + 1;
+            while (j == excludedIndex && j < report.Length)
+            {
+                j++;
+            }
+
+            if (j >= report.Length)
+            {
+                break;
+            }
+
+            int currentDifference = report[j] - report[i];
+
+            if (Math.Abs(currentDifference) < 1 || Math.Abs(currentDifference) > 3)
+            {
+                return false;
+            }
+
+            if (i != 0 && previousDifference * currentDifference < 0)
+            {
+                return false;
+            }
+
+            previousDifference = currentDifference;
+        }
+
+        return true;
+    }
+
+    private static bool IsReportSafeWithoutAdjustment(int[] report)
+    {
+        return IsReportSafe(report, -1);
+    }
+
+    private static bool IsReportSafeWithOneAdjustment(int[] report)
+    {
+        if (IsReportSafe(report, -1))
+        {
+            return true;
+        }
+
+        for (int i = 0; i < report.Length; i++)
+        {
+            if (IsReportSafe(report, i))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static void ProcessReportsAndCountSafeOnes(string inputFileName, string outputFileName, Func<int[], bool> isReportSafeFunc)
+    {
+        int safeReportsCount = 0;
+
+        try
+        {
+            using (var reader = new StreamReader(inputFileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string? line = reader.ReadLine();
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        continue;
+                    }
+
+                    var report = line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+                                     .Select(int.Parse)
+                                     .ToArray();
+
+                    if (isReportSafeFunc(report))
+                    {
+                        safeReportsCount++;
+                    }
+                }
+            }
+
+            using (var writer = new StreamWriter(outputFileName))
+            {
+                writer.WriteLine(safeReportsCount);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while processing the file '{inputFileName}': {ex.Message}");
+        }
+    }
+}
